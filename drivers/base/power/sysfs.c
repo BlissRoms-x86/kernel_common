e0419c265e0db63951b5a034d509dd0a412b8bb1
Derek Basehore <dbasehore@chromium.org>
2013-04-03 11:06:33 -0700

    CHROMIUM: Separate dark resume into its own sysfs attr group
    This change separates the dark resume sysfs files into their own attribute
    group. This way, we can add those sysfs files to only the devices that have a
    driver that can make use of them. This also allows for adding the sysfs files to
    devices whose drivers did not add the wakeup attribute group which the dark
    resume sysfs files were part of before.
    
    Dark resume functionality is also moved from drivers/base/power/main.c to its
    own file.
    
    BUG=chromium:226157
    TEST=power_DarkResumeShutdownServer (has to be run in lab)
    
    Change-Id: I916ca6208bf26dd5269ab288169f5d802996e3d9
    [sonnyrao: 3.8: fixed up conflicts]
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/47287
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/57404
    
    Conflicts:
    	drivers/base/power/Makefile
    [filbranden: 3.18 rebase. Solved conflicts, renamed {en,dis}abled with _.]
    Signed-off-by: Filipe Brandenburger <filbranden@chromium.org>
    
    Conflicts:
    	include/linux/pm.h
    	drivers/base/power/power.h
    	drivers/base/power/Makefile
    	drivers/rtc/rtc-cmos.c
    
    [rebase44(groeck):
    	Resolved conflicts.
    	Dropped changes in drivers/rtc/rtc-cmos.c.]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/base/power/sysfs.c
+++ drivers/base/power/sysfs.c
@@ -5,6 +5,7 @@
 #include <linux/device.h>
 #include <linux/string.h>
 #include <linux/export.h>
+#include <linux/pm_dark_resume.h>
 #include <linux/pm_qos.h>
 #include <linux/pm_runtime.h>
 #include <linux/atomic.h>
@@ -539,9 +540,9 @@ static ssize_t dark_resume_active_store(struct device *dev,
 				const char *buf, size_t n)
 {
 	if (sysfs_streq(_enabled, buf))
-		dev->power.use_dark_resume = true;
+		dev_dark_resume_set_active(dev, true);
 	else if (sysfs_streq(_disabled, buf))
-		dev->power.use_dark_resume = false;
+		dev_dark_resume_set_active(dev, false);
 	else
 		return -EINVAL;
 	return n;
@@ -553,7 +554,7 @@ static DEVICE_ATTR(dark_resume_active, 0644,
 static ssize_t dark_resume_source_show(struct device *dev,
 				struct device_attribute *attr, char *buf)
 {
-	return dev->power.dpd && dev->power.dpd->is_source ?
+	return dev->power.dark_resume && dev->power.dark_resume->is_source ?
 			sprintf(buf, "%s\n", _enabled) :
 			sprintf(buf, "%s\n", _disabled);
 }
@@ -562,10 +563,10 @@ static ssize_t dark_resume_source_store(struct device *dev,
 				struct device_attribute *attr,
 				const char *buf, size_t n)
 {
-	if (sysfs_streq(_enabled, buf) && dev->power.dpd)
-		dpm_set_dark_source(dev->power.dpd, true);
-	else if (sysfs_streq(_disabled, buf) && dev->power.dpd)
-		dpm_set_dark_source(dev->power.dpd, false);
+	if (sysfs_streq(_enabled, buf) && dev->power.dark_resume)
+		dev_dark_resume_set_source(dev, true);
+	else if (sysfs_streq(_disabled, buf) && dev->power.dark_resume)
+		dev_dark_resume_set_source(dev, false);
 	else
 		return -EINVAL;
 	return n;
@@ -670,14 +671,24 @@ static struct attribute *wakeup_attrs[] = {
 #ifdef CONFIG_PM_AUTOSLEEP
 	&dev_attr_wakeup_prevent_sleep_time_ms.attr,
 #endif
-	&dev_attr_dark_resume_active.attr,
-	&dev_attr_dark_resume_source.attr,
 #endif /* CONFIG_PM_SLEEP */
 	NULL,
 };
 static struct attribute_group pm_wakeup_attr_group = {
 	.name	= power_group_name,
 	.attrs	= wakeup_attrs,
+};
+
+static struct attribute *dark_resume_attrs[] = {
+#ifdef CONFIG_PM_SLEEP
+	&dev_attr_dark_resume_active.attr,
+	&dev_attr_dark_resume_source.attr,
+#endif
+	NULL,
+};
+static struct attribute_group pm_dark_resume_attr_group = {
+	.name	= power_group_name,
+	.attrs	= dark_resume_attrs,
 };
 
 static struct attribute *runtime_attrs[] = {
@@ -811,4 +822,14 @@ void dpm_sysfs_remove(struct device *dev)
 	rpm_sysfs_remove(dev);
 	sysfs_unmerge_group(&dev->kobj, &pm_wakeup_attr_group);
 	sysfs_remove_group(&dev->kobj, &pm_attr_group);
+}
+
+int dark_resume_sysfs_add(struct device *dev)
+{
+	return sysfs_merge_group(&dev->kobj, &pm_dark_resume_attr_group);
+}
+
+void dark_resume_sysfs_remove(struct device *dev)
+{
+	sysfs_unmerge_group(&dev->kobj, &pm_dark_resume_attr_group);
 }
