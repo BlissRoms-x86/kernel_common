cbf7f4fb198aed112b45033ef93c190e26a5f33c
Zach Reizner <zachr@chromium.org>
2014-11-05 10:34:06 -0800

    CHROMIUM: drm/cirrus: implement PRIME export for cirrus
    This patch implements PRIME export, but not import for cirrus.
    
    BUG=chromium:394868
    TEST=None
    
    Signed-off-by: Zach Reizner <zachr@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/229688
    Reviewed-by: Stphane Marchesin <marcheu@chromium.org>
    (cherry picked from commit 63aa11e34f89c37d63ebcb06698b3da14c172a34)
    Signed-off-by: Stphane Marchesin <marcheu@chromium.org>
    (cherry picked from commit 5825a731b17e0db20c7db4283e7007255ddb86c8)
    
    Change-Id: I9c930853aa99aaea40496006f111612d33cc0bce
    Reviewed-on: https://chromium-review.googlesource.com/339057
    Commit-Ready: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/cirrus/cirrus_drv.h
+++ drivers/gpu/drm/cirrus/cirrus_drv.h
@@ -164,6 +164,7 @@ struct cirrus_bo {
 	struct ttm_buffer_object bo;
 	struct ttm_placement placement;
 	struct ttm_bo_kmap_obj kmap;
+	struct ttm_bo_kmap_obj dma_buf_vmap;
 	struct drm_gem_object gem;
 	struct ttm_place placements[3];
 	int pin_count;
@@ -262,6 +263,14 @@ static inline void cirrus_bo_unreserve(struct cirrus_bo *bo)
 
 int cirrus_bo_push_sysram(struct cirrus_bo *bo);
 int cirrus_bo_pin(struct cirrus_bo *bo, u32 pl_flag, u64 *gpu_addr);
+
+struct sg_table *cirrus_gem_prime_get_sg_table(struct drm_gem_object *obj);
+void *cirrus_gem_prime_vmap(struct drm_gem_object *obj);
+void cirrus_gem_prime_vunmap(struct drm_gem_object *obj, void *vaddr);
+struct drm_gem_object *cirrus_gem_prime_import_sg_table(struct drm_device *dev,
+							size_t size,
+							struct sg_table *sg);
+int cirrus_gem_prime_pin(struct drm_gem_object *obj);
 
 extern int cirrus_bpp;
 
