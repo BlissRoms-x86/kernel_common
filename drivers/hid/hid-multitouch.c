750dd719a0a0caa2bde1b879ff15f2474042d0e5
Daniel Martin <consume.noise@gmail.com>
2015-10-02 10:05:29 +0200

    [PATCH 1/4] HID: multitouch: Ignore invalid reports
    Drop reports with invalid values.
    
    Signed-off-by: Daniel Martin <consume.noise@gmail.com>
    
    Conflicts:
    	drivers/hid/hid-multitouch.c

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -636,14 +636,6 @@ static void mt_complete_slot(struct mt_device *td, struct input_dev *input)
 	    td->curdata.w == 0xffff && td->curdata.w == td->curdata.h)
 		goto inc_num_received;
 
-	if (td->curdata.x == 0xffff && td->curdata.x == td->curdata.y &&
-	    td->curdata.w == 0xffff && td->curdata.w == td->curdata.h)
-		goto inc_num_received;
-
-	if (td->curdata.x == 0xffff && td->curdata.x == td->curdata.y &&
-	    td->curdata.w == 0xffff && td->curdata.w == td->curdata.h)
-		goto inc_num_received;
-
 	if (td->curvalid || (td->mtclass.quirks & MT_QUIRK_ALWAYS_VALID)) {
 		int active;
 		int slotnum = mt_compute_slot(td, input);
@@ -686,7 +678,6 @@ static void mt_complete_slot(struct mt_device *td, struct input_dev *input)
 			input_event(input, EV_ABS, ABS_MT_TOUCH_MINOR, minor);
 		}
 	}
-
 inc_num_received:
 	td->num_received++;
 }
