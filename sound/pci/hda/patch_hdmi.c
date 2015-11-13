5033898ba1ccf5e192c4bbd5312215b282c37320
Takashi Iwai <tiwai@suse.de>
2015-11-13 12:51:49 +0100

    UPSTREAM: ALSA: hda - Do zero-clear in snd_hdmi_parse_eld() itself
    Instead of doing in each caller side, snd_hdmi_parse_eld() does
    zero-clear of the parsed data by itself.  This is safer and simplifies
    the upcoming code changes.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    From v4.5-rc1
    (cherry picked from commit 18014fd793d5e73eec5f2c22eaa37a32b44748eb)
    Signed-off-by: Vedang Patel <vedang.patel@intel.com>
    
    BUG=chrome-os-partner:53967
    TEST=with the whole series applied, Audio works on Amenia.
    
    Change-Id: I6e7a3ce499bdace82ed16032cc43d3fc3ffe42cc
    Reviewed-on: https://chromium-review.googlesource.com/347499
    Commit-Ready: Vedang Patel <vedang.patel@intel.com>
    Tested-by: Russ Sage <russ.sage@intel.com>
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- sound/pci/hda/patch_hdmi.c
+++ sound/pci/hda/patch_hdmi.c
@@ -1624,7 +1624,6 @@ static bool hdmi_present_sense_via_verbs(struct hdmi_spec_per_pin *per_pin,
 						     &eld->eld_size) < 0)
 			eld->eld_valid = false;
 		else {
-			memset(&eld->info, 0, sizeof(struct parsed_hdmi_eld));
 			if (snd_hdmi_parse_eld(codec, &eld->info, eld->eld_buffer,
 						    eld->eld_size) < 0)
 				eld->eld_valid = false;
