c9914e16e95f151034ce3755ffaebbf15809222e
Ville Syrjälä <ville.syrjala@linux.intel.com>
2015-12-08 19:59:38 +0200

    UPSTREAM: drm/i915: Store max lane count in intel_digital_port
    Rather than having open coded checks for the DDI A/E configuration,
    just store the max supported lane count in intel_digital_port.
    
    We had an open coded check for DDI A, but not for DDI E. So we may
    have been vilating the DDI E max lane count.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit ccb1a8319015d31a51e8206b66ab3aaea07417ba
    from v4.6-rc2 )
    Signed-off-by: Tarun Vyas <tarun.vyas@intel.com>
    
    Change-Id: I6484709f034e4bf770a7ee356fe2335887676e46

--- drivers/gpu/drm/i915/intel_hdmi.c
+++ drivers/gpu/drm/i915/intel_hdmi.c
@@ -2033,6 +2033,11 @@ void intel_hdmi_init_connector(struct intel_digital_port *intel_dig_port,
 	enum port port = intel_dig_port->port;
 	uint8_t alternate_ddc_pin;
 
+	if (WARN(intel_dig_port->max_lanes < 4,
+		 "Not enough lanes (%d) for HDMI on port %c\n",
+		 intel_dig_port->max_lanes, port_name(port)))
+		return;
+
 	drm_connector_init(dev, connector, &intel_hdmi_connector_funcs,
 			   DRM_MODE_CONNECTOR_HDMIA);
 	drm_connector_helper_add(connector, &intel_hdmi_connector_helper_funcs);
@@ -2216,6 +2221,7 @@ void intel_hdmi_init(struct drm_device *dev,
 	intel_dig_port->port = port;
 	intel_dig_port->hdmi.hdmi_reg = hdmi_reg;
 	intel_dig_port->dp.output_reg = INVALID_MMIO_REG;
+	intel_dig_port->max_lanes = 4;
 
 	intel_hdmi_init_connector(intel_dig_port, intel_connector);
 }
