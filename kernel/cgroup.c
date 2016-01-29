606df679289378097a0e01603aae0e8db280383e
Serge Hallyn <serge.hallyn@ubuntu.com>
2016-01-29 02:54:11 -0600

    BACKPORT: Add FS_USERNS_FLAG to cgroup fs
    allowing root in a non-init user namespace to mount it.  This should
    now be safe, because
    
    1. non-init-root cannot mount a previously unbound subsystem
    2. the task doing the mount must be privileged with respect to the
       user namespace owning the cgroup namespace
    3. the mounted subsystem will have its current cgroup as the root dentry.
       the permissions will be unchanged, so tasks will receive no new
       privilege over the cgroups which they did not have on the original
       mounts.
    
    Signed-off-by: Serge Hallyn <serge.hallyn@canonical.com>
    
    BUG=b:29259708
    TEST=Built and booted on cyan
    
    Conflicts:
    	kernel/cgroup.c
    
    because we do not have cgroup v2 support.
    
    (cherry picked from commit 1c53753e0df1ae4d21661053459e7c024a43f1d3)
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    
    Change-Id: I803333387e01b039f26e7a2a17bb4d036d8e1ac3
    Reviewed-on: https://chromium-review.googlesource.com/353798
    Commit-Ready: Dmitry Torokhov <dtor@chromium.org>
    Tested-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    (cherry picked from commit 5b55262960c7bb78ef01729bbfd1f1c1d24a0068)
    Reviewed-on: https://chromium-review.googlesource.com/354507

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2244,6 +2244,7 @@ static struct file_system_type cgroup_fs_type = {
 	.name = "cgroup",
 	.mount = cgroup_mount,
 	.kill_sb = cgroup_kill_sb,
+	.fs_flags = FS_USERNS_MOUNT,
 };
 
 static char *cgroup_path_ns_locked(struct cgroup *cgrp, char *buf, size_t buflen,
