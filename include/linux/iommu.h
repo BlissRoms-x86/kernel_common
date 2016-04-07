67872502ec3f94bc365f089781beddec2c124fd2
Will Deacon <will.deacon@arm.com>
2016-04-07 18:42:04 +0100

    UPSTREAM: iommu: remove unused priv field from struct iommu_ops
    The priv field from iommu_ops is a hangover from the of_dma_configure
    series and isn't actually used. Remove it before it has chance to
    spread.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    (cherry picked from commit 3c3e8943ac6f36ca5d18ca61b30634fb560b4ebb)
    
    BUG=chrome-os-partner:55139
    TEST=Build
    
    Change-Id: I3552bf4673ca9aa40610e971eeed447d087c9ccd
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/363211
    Commit-Ready: Tomasz Figa <tfiga@chromium.org>
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -148,7 +148,6 @@ struct iommu_dm_region {
  * @domain_set_attr: Change domain attributes
  * @of_xlate: add OF master IDs to iommu grouping
  * @pgsize_bitmap: bitmap of supported page sizes
- * @priv: per-instance data private to the iommu driver
  */
 struct iommu_ops {
 	bool (*capable)(enum iommu_cap);
@@ -192,7 +191,6 @@ struct iommu_ops {
 #endif
 
 	unsigned long pgsize_bitmap;
-	void *priv;
 };
 
 #define IOMMU_GROUP_NOTIFY_ADD_DEVICE		1 /* Device added */
