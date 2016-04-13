54d2d0a7eb6571369845f3be2c3b77d5c115a852
Yakir Yang <ykk@rock-chips.com>
2016-04-13 12:06:59 +0800

    FROMLIST: drm/rockchip: analogix_dp: correct the connector display color format and bpc
    Rockchip VOP couldn't output YUV video format for eDP controller, so
    when driver detect connector support YUV video format, we need to hack
    it down to RGB888.
    
    BUG=chrome-os-partner:53565
    TEST=Test eDP on rk3399-gru
    
    Change-Id: I1f6a61773ea8014d2de4e2efb597b36c273e4bb3
    Acked-by: Mark Yao <mark.yao@rock-chips.com>
    Signed-off-by: Yakir Yang <ykk@rock-chips.com>
    (am from https://patchwork.kernel.org/patch/9175601/)
    Reviewed-on: https://chromium-review.googlesource.com/346318
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
+++ drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
@@ -102,6 +102,22 @@ static int rockchip_dp_powerdown(struct analogix_dp_plat_data *plat_data)
 	return 0;
 }
 
+static int rockchip_dp_get_modes(struct analogix_dp_plat_data *plat_data,
+				 struct drm_connector *connector)
+{
+	struct drm_display_info *di = &connector->display_info;
+
+	if (di->color_formats & DRM_COLOR_FORMAT_YCRCB444 ||
+	    di->color_formats & DRM_COLOR_FORMAT_YCRCB422) {
+		di->color_formats &= ~(DRM_COLOR_FORMAT_YCRCB422 |
+				       DRM_COLOR_FORMAT_YCRCB444);
+		di->color_formats |= DRM_COLOR_FORMAT_RGB444;
+		di->bpc = 8;
+	}
+
+	return 0;
+}
+
 static bool
 rockchip_dp_drm_encoder_mode_fixup(struct drm_encoder *encoder,
 				   const struct drm_display_mode *mode,
@@ -313,6 +329,7 @@ static int rockchip_dp_bind(struct device *dev, struct device *master,
 	dp->plat_data.subdev_type = dp_data->chip_type;
 	dp->plat_data.power_on = rockchip_dp_poweron;
 	dp->plat_data.power_off = rockchip_dp_powerdown;
+	dp->plat_data.get_modes = rockchip_dp_get_modes;
 
 	return analogix_dp_bind(dev, dp->drm_dev, &dp->plat_data);
 }
