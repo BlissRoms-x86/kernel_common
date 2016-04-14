5eb9028c61ca13b1c0a3eda5884e6e2f8d6ddbac
Stéphane Marchesin <marcheu@chromium.org>
2016-04-14 16:09:34 -0700

    CHROMIUM: drm/i915: Always disable the main link when in PSR.
    The standby behavior is not clearly specified in the VESA standard. This
    leads to each TCON vendor implementing their own interpretation.
    Currently, the Samus EVT TCON does not follow the standy behavior as
    configured by the code. Moreover, to get the most power gain, the main
    link should be disabled rather than put in standby.
    
    BUG=chrome-os-partner:31836
    TEST=Build kernel and push to Samus. Enable PSR with
    'xrandr --output eDP1 --set psr on'. The display should not freeze.
    Signed-off-by: Siva Chandra <sivachandra@chromium.org>
    
    Change-Id: I46d8af5fbff0a21b426efb84c3fdbad823d8fd8f
    Signed-off-by: Stphane Marchesin <marcheu@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/339098
    Commit-Ready: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_psr.c
+++ drivers/gpu/drm/i915/intel_psr.c
@@ -266,16 +266,6 @@ static void hsw_psr_enable_source(struct intel_dp *intel_dp)
 	uint32_t val = 0x0;
 	const uint32_t link_entry_time = EDP_PSR_MIN_LINK_ENTRY_TIME_8_LINES;
 
-	if (intel_dp->psr_dpcd[1] & DP_PSR_NO_TRAIN_ON_EXIT) {
-		/* It doesn't mean we shouldn't send TPS patters, so let's
-		   send the minimal TP1 possible and skip TP2. */
-		val |= EDP_PSR_TP1_TIME_100us;
-		val |= EDP_PSR_TP2_TP3_TIME_0us;
-		val |= EDP_PSR_SKIP_AUX_EXIT;
-		/* Sink should be able to train with the 5 or 6 idle patterns */
-		idle_frames += 4;
-	}
-
 	I915_WRITE(EDP_PSR_CTL(dev), val |
 		   (IS_BROADWELL(dev) ? 0 : link_entry_time) |
 		   max_sleep_time << EDP_PSR_MAX_SLEEP_TIME_SHIFT |
