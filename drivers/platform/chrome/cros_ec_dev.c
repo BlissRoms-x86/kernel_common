35a1e4401dc5e49ad3db9ad581816296808637fa
Gwendal Grignou <gwendal@chromium.org>
2016-04-22 11:04:30 -0700

    CHROMIUM: Revert "cros_ec: Remove unused ioctl CROS_EC_DEV_IOCRDMEM"
    Removing CROS_EC_DEV_IOCRDMEM is not possible.
    On some architecture reading legacy IO mapped memory register is done
    through EMI indirect access.
    To make it transparent from the user space, re-add the ioctl.
    
    TEST=Compile.
    BUG=chrome-os-partner:52550,chromium:602832
    
    This reverts commit c0567473579e ("cros_ec: Remove unused ioctl
    CROS_EC_DEV_IOCRDMEM")
    
    Change-Id: Id09a62b52290a7824998fe3a6729b9c520602992
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/340296
    Reviewed-by: Guenter Roeck <groeck@google.com>

--- drivers/platform/chrome/cros_ec_dev.c
+++ drivers/platform/chrome/cros_ec_dev.c
@@ -169,6 +169,30 @@ exit:
 	return ret;
 }
 
+static long ec_device_ioctl_readmem(struct cros_ec_dev *ec, void __user *arg)
+{
+	struct cros_ec_device *ec_dev = ec->ec_dev;
+	struct cros_ec_readmem s_mem = { };
+	long num;
+
+	/* Not every platform supports direct reads */
+	if (!ec_dev->cmd_readmem)
+		return -ENOTTY;
+
+	if (copy_from_user(&s_mem, arg, sizeof(s_mem)))
+		return -EFAULT;
+
+	num = ec_dev->cmd_readmem(ec_dev, s_mem.offset, s_mem.bytes,
+				  s_mem.buffer);
+	if (num <= 0)
+		return num;
+
+	if (copy_to_user((void __user *)arg, &s_mem, sizeof(s_mem)))
+		return -EFAULT;
+
+	return num;
+}
+
 static long ec_device_ioctl(struct file *filp, unsigned int cmd,
 			    unsigned long arg)
 {
@@ -180,6 +204,8 @@ static long ec_device_ioctl(struct file *filp, unsigned int cmd,
 	switch (cmd) {
 	case CROS_EC_DEV_IOCXCMD:
 		return ec_device_ioctl_xcmd(ec, (void __user *)arg);
+	case CROS_EC_DEV_IOCRDMEM:
+		return ec_device_ioctl_readmem(ec, (void __user *)arg);
 	}
 
 	return -ENOTTY;
