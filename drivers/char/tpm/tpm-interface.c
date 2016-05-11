37a4f05cec5ea0892aedd34e343ef060d81874b7
Stefan Berger <stefanb@linux.vnet.ibm.com>
2016-05-11 11:28:27 -0400

    UPSTREAM: tpm: Fix suspend regression
    Fix the suspend regression due to the wrong way of retrieving the
    chip structure. The suspend functions are attached to the hardware
    device, not the chip and thus must rely on drvdata.
    
    BUG=chrome-os-partner:54590
    TEST=Build and boot on kevin, verify that /dev/tpm0 is present.
    
    Fixes: e89f8b1ade9cc1a ("tpm: Remove all uses of drvdata from the TPM Core")
    Reported-by: Jeremiah Mahler <jmmahler@gmail.com>
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Tested-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Reviewed-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    Tested-by: Jeremiah Mahler <jmmahler@gmail.com>
    Acked-by: Jarkko Sakkinen <jarkko.sakkine@linux.intel.com>
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkine@linux.intel.com>
    (cherry picked from commit e8f2f45a4402deb715220b750d892f7a70b0f868
     git://git.infradead.org/users/jjs/linux-tpmdd.git master)
    Signed-off-by: Andrey Pronin <apronin@chromium.org>
    
    Change-Id: Idbc3d87e3e18c70c06995a91ad79606e30f7a22d
    Reviewed-on: https://chromium-review.googlesource.com/354876
    Commit-Ready: Andrey Pronin <apronin@chromium.org>
    Tested-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- drivers/char/tpm/tpm-interface.c
+++ drivers/char/tpm/tpm-interface.c
@@ -983,7 +983,7 @@ static struct tpm_input_header savestate_header = {
  */
 int tpm_pm_suspend(struct device *dev)
 {
-	struct tpm_chip *chip = to_tpm_chip(dev);
+	struct tpm_chip *chip = dev_get_drvdata(dev);
 	struct tpm_cmd_t cmd;
 	int rc, try;
 
@@ -1044,7 +1044,7 @@ EXPORT_SYMBOL_GPL(tpm_pm_suspend);
  */
 int tpm_pm_resume(struct device *dev)
 {
-	struct tpm_chip *chip = to_tpm_chip(dev);
+	struct tpm_chip *chip = dev_get_drvdata(dev);
 
 	if (chip == NULL)
 		return -ENODEV;
