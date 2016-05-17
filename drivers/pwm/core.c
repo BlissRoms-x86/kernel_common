dc4062524a4077e2bb1c009f0b8f3358e50db044
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-05-17 14:27:25 +0200

    UPSTREAM: pwm: Fix pwm_apply_args() call sites
    pwm_apply_args() is supposed to initialize a PWM device according to the
    arguments provided by the DT or the PWM lookup, but this function was
    called inside pwm_device_request(), which in turn was called before the
    core had a chance to initialize the pwm->args fields.
    
    Fix that by calling pwm_apply_args directly in pwm_get() and of_pwm_get()
    after initializing pwm->args field.
    
    This commit also fixes an invalid pointer dereference introduced by
    commit e39c0df1be5a ("pwm: Introduce the pwm_args concept").
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Fixes: e39c0df1be5a ("pwm: Introduce the pwm_args concept")
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>
    (cherry picked from git://git.kernel.org/pub/scm/linux/kernel/git/thierry.reding/linux-pwm.git for-next
     commit fbd45a12988e75a48d392feb8b0e5feb5d612513)
    
    BUG=chrome-os-partner:52666
    TEST=build and boot for gru/kevin
    
    Change-Id: I2fbf61c848adb68b4860d21fe7c80da336eb99fb
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/345589
    Reviewed-by: Stephen Barber <smbarber@chromium.org>

--- drivers/pwm/core.c
+++ drivers/pwm/core.c
@@ -669,6 +669,13 @@ struct pwm_device *of_pwm_get(struct device_node *np, const char *con_id)
 
 	pwm->label = con_id;
 
+	/*
+	 * FIXME: This should be removed once all PWM users properly make use
+	 * of struct pwm_args to initialize the PWM device. As long as this is
+	 * here, the PWM state and hardware state can get out of sync.
+	 */
+	pwm_apply_args(pwm);
+
 put:
 	of_node_put(args.np);
 
@@ -796,12 +803,19 @@ struct pwm_device *pwm_get(struct device *dev, const char *con_id)
 	if (!chip)
 		goto out;
 
-	pwm->args.period = chosen->period;
-	pwm->args.polarity = chosen->polarity;
-
 	pwm = pwm_request_from_chip(chip, chosen->index, con_id ?: dev_id);
 	if (IS_ERR(pwm))
 		goto out;
+
+	pwm->args.period = chosen->period;
+	pwm->args.polarity = chosen->polarity;
+
+	/*
+	 * FIXME: This should be removed once all PWM users properly make use
+	 * of struct pwm_args to initialize the PWM device. As long as this is
+	 * here, the PWM state and hardware state can get out of sync.
+	 */
+	pwm_apply_args(pwm);
 
 out:
 	mutex_unlock(&pwm_lookup_lock);
