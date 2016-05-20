b70a3434efa7da2694e03b3bc1bbf0cd6823ea1a
Caesar Wang <wxt@rock-chips.com>
2016-05-20 16:40:29 +0800

    CHROMIUM: MALI: platform/rk: update the specific code
    We use devfreq to implement DVFS of GPU, instead of 'legacy_dvfs'.
    Add the rockchip platform for rk_pm_callback_conf.
    
    BUG=chrome-os-partner:51368
    TEST=tested the gpu for kevin;
    TEST=See /sys/class/devfreq/ff9a0000.gpu
    [    2.482504] mali ff9a0000.gpu: GPU identified as 0x0860 r2p0 status 0
    [    2.508316] mali ff9a0000.gpu: Probed as mali0
    
    Change-Id: I35cfcacc2e1d970d896aa6f66a850fb1746b87e8
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/346375
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/gpu/arm/midgard/platform/rk/mali_kbase_config_platform.h
+++ drivers/gpu/arm/midgard/platform/rk/mali_kbase_config_platform.h
@@ -1,47 +1,57 @@
 /*
- * (C) COPYRIGHT 2014 ARM Limited. All rights reserved.
+ * (C) COPYRIGHT 2014-2016 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
  * Foundation, and any use by you of this program is subject to the terms
  * of such GNU licence.
- *
- * A copy of the licence is included with the program, and can also be obtained
- * from Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
- *
  */
 
-#include "mali_kbase_rk.h"
-
 /**
- * Maximum frequency GPU will be clocked at. Given in kHz.
+ * @file mali_kbase_config_platform.h
+ * That define the RK platform config.
  */
-#define GPU_FREQ_KHZ_MAX KBASE_RK_GPU_FREQ_KHZ_MAX
 
 /**
- * Minimum frequency GPU will be clocked at. Given in kHz.
- */
-#define GPU_FREQ_KHZ_MIN KBASE_RK_GPU_FREQ_KHZ_MIN
-
-/**
- * A pointer to a function that calculates the CPU clock
- * speed of the platform in MHz - see
- * @ref kbase_cpuprops_clock_speed_function for the function
- * prototype.
+ * Maximum frequency GPU will be clocked at.
+ * Given in kHz.
+ * This must be specified as there is no default value.
  *
- * Attached value: A @ref kbase_cpuprops_clock_speed_function.
+ * Attached value: number in kHz
+ * Default value: NA
+ */
+#define GPU_FREQ_KHZ_MAX (500000)
+
+/**
+ * Minimum frequency GPU will be clocked at.
+ * Given in kHz.
+ * This must be specified as there is no default value.
+ *
+ * Attached value: number in kHz
+ * Default value: NA
+ */
+#define GPU_FREQ_KHZ_MIN (500000)
+
+/**
+ * CPU_SPEED_FUNC
+ * - A pointer to a function that calculates the CPU clock
+ *
+ * CPU clock speed of the platform is in MHz
+ * - see kbase_cpu_clk_speed_func for the function prototype.
+ *
+ * Attached value: A kbase_cpu_clk_speed_func.
  * Default Value:  NA
  */
-#define CPU_SPEED_FUNC (&kbase_cpuprops_get_default_clock_speed)
+#define CPU_SPEED_FUNC (NULL)
 
 /**
- * A pointer to a function that calculates the GPU clock
- * speed of the platform in MHz - see
- * @ref kbase_gpuprops_clock_speed_function for the function
- * prototype.
+ * GPU_SPEED_FUNC
+ * - A pointer to a function that calculates the GPU clock
  *
- * Attached value: A @ref kbase_gpuprops_clock_speed_function.
+ * GPU clock speed of the platform in MHz
+ * - see kbase_gpu_clk_speed_func for the function prototype.
+ *
+ * Attached value: A kbase_gpu_clk_speed_func.
  * Default Value:  NA
  */
 #define GPU_SPEED_FUNC (NULL)
@@ -49,25 +59,24 @@
 /**
  * Power management configuration
  *
- * Attached value: pointer to @ref kbase_pm_callback_conf
- * Default value: See @ref kbase_pm_callback_conf
+ * Attached value:
+ *	pointer to @ref kbase_pm_callback_conf
+ * Default value:
+ *	See @ref kbase_pm_callback_conf
  */
-#define POWER_MANAGEMENT_CALLBACKS (&kbase_rk_pm_callbacks)
+#define POWER_MANAGEMENT_CALLBACKS (&pm_callbacks)
+extern struct kbase_pm_callback_conf pm_callbacks;
 
 /**
  * Platform specific configuration functions
  *
- * Attached value: pointer to @ref kbase_platform_funcs_conf
- * Default value: See @ref kbase_platform_funcs_conf
+ * Attached value:
+ *	pointer to @ref kbase_platform_funcs_conf
+ * Default value:
+ *	See @ref kbase_platform_funcs_conf
  */
-#define PLATFORM_FUNCS (&kbase_rk_platform_funcs)
-
-/**
- * Power model for IPA
- *
- * Attached value: pointer to @ref mali_pa_model_ops
- */
-#define POWER_MODEL_CALLBACKS (NULL)
+#define PLATFORM_FUNCS (&platform_funcs)
+extern struct kbase_platform_funcs_conf platform_funcs;
 
 /**
  * Secure mode switch
@@ -76,5 +85,3 @@
  */
 #define SECURE_CALLBACKS (NULL)
 
-extern struct kbase_pm_callback_conf kbase_rk_pm_callbacks;
-extern struct kbase_platform_funcs_conf kbase_rk_platform_funcs;
--- drivers/gpu/arm/midgard/platform/rk/mali_kbase_rk.h
+++ drivers/gpu/arm/midgard/platform/rk/mali_kbase_rk.h
@@ -1,41 +1,30 @@
-/*
- * Rockchip SoC Mali-T764 platform-dependent codes
+/* drivers/gpu/t6xx/kbase/src/platform/rk/mali_kbase_platform.h
+ * Rockchip SoC Mali-Midgard platform-dependent codes
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software FoundatIon.
  */
 
-#ifndef _KBASE_PLATFORM_H_
-#define _KBASE_PLATFORM_H_
+/**
+ * @file mali_kbase_rk.h
+ *
+ * defines work_context type of platform_dependent_part.
+ */
 
-#include <linux/clk.h>
-#include <mali_kbase.h>
-#include "mali_kbase_rk_dvfs.h"
+#ifndef _MALI_KBASE_RK_H_
+#define _MALI_KBASE_RK_H_
 
-#define KBASE_RK_GPU_FREQ_KHZ_MAX               600000
-#define KBASE_RK_GPU_FREQ_KHZ_MIN               100000
+/*---------------------------------------------------------------------------*/
 
-struct kbase_rk_fv {
-	unsigned long freq;
-	unsigned long volt;
-};
-
-struct kbase_rk {
-	struct kbase_device *kbdev;
-	struct clk *clk;
-	struct regulator *regulator;
-	struct kbase_rk_fv *fv_table;
-	struct notifier_block thermal_change_nb;
-	unsigned int fv_table_length;
-	unsigned int current_level;
-	unsigned int requested_level;
-	unsigned int thermal_throttling_level;
-	struct kbase_rk_dvfs dvfs;
-	struct mutex set_level_lock;
+/**
+ * struct rk_context - work_context of platform_dependent_part_of_rk.
+ * @is_powered: record the status
+ *      of common_parts calling 'power_on_callback' and 'power_off_callback'.
+ */
+struct rk_context {
 	bool is_powered;
 };
 
-int kbase_rk_set_freq(struct kbase_device *kbdev, unsigned long freq);
+#endif				/* _MALI_KBASE_RK_H_ */
 
-#endif				/* _KBASE_PLATFORM_H_ */
