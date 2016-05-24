0ec8d386c56abc2a5261729cb23eba03f9be4fc8
Chinyue Chen <chinyue@chromium.org>
2016-05-24 14:37:50 +0800

    CHROMIUM: alt-syscall: Whitelist waitid(2) for Android.
    waitid(2) is required by CTS.
    
    BUG=b:27177099
    BUG=b:29554103
    TEST=Tested on minnie with CTS performance test.
    
    Signed-off-by: Chinyue Chen <chinyue@chromium.org>
    Originally-Reviewed-on: https://chromium-review.googlesource.com/346881
    
    Change-Id: I17e762fa05da6361c0819b0af0612192d8838df7
    Reviewed-on: https://chromium-review.googlesource.com/355412
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- security/chromiumos/alt-syscall.c
+++ security/chromiumos/alt-syscall.c
@@ -338,6 +338,7 @@ static asmlinkage long alt_sys_prctl(int option, unsigned long arg2,
 #define __NR_compat_vfork	__NR_ia32_vfork
 #define __NR_compat_vmsplice	__NR_ia32_vmsplice
 #define __NR_compat_wait4	__NR_ia32_wait4
+#define __NR_compat_waitid	__NR_ia32_waitid
 #define __NR_compat_waitpid	__NR_ia32_waitpid
 #define __NR_compat_write	__NR_ia32_write
 #define __NR_compat_writev	__NR_ia32_writev
@@ -620,6 +621,7 @@ static struct syscall_whitelist_entry android_whitelist[] = {
 	SYSCALL_ENTRY(utimensat),
 	SYSCALL_ENTRY(vmsplice),
 	SYSCALL_ENTRY(wait4),
+	SYSCALL_ENTRY(waitid),
 	SYSCALL_ENTRY(write),
 	SYSCALL_ENTRY(writev),
 
@@ -979,6 +981,7 @@ static struct syscall_whitelist_entry android_compat_whitelist[] = {
 	COMPAT_SYSCALL_ENTRY(vfork),
 	COMPAT_SYSCALL_ENTRY(vmsplice),
 	COMPAT_SYSCALL_ENTRY(wait4),
+	COMPAT_SYSCALL_ENTRY(waitid),
 	COMPAT_SYSCALL_ENTRY(write),
 	COMPAT_SYSCALL_ENTRY(writev),
 	COMPAT_SYSCALL_ENTRY(chown32),
