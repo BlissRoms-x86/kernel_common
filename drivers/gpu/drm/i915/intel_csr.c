d6d90ae089a5bbfce4661c21fa11e799de46af62
Marc Herbert <marc.herbert@intel.com>
2016-06-20 16:07:40 -0700

    CHROMIUM: drm/i915/gen9: wait until SYSTEM_RUNNING before loading DMC firmware.
    The Linux kernel has no generic support for built-in driver + firmware(s)
    located on the root filesystem.
    This is an interim workaround until a final solution is decided upstream.
    Acked by the i915 maintainer at:
      http://lists.freedesktop.org/archives/intel-gfx/2015-July/071606.html
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>
    Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    
    Conflicts:
    	drivers/gpu/drm/i915/intel_csr.c
    	Fixed context delta.
    
    FIX for chromeos-4.4: _wait_for(milliseconds) became _wait_for(microseconds)
    https://chromium-review.googlesource.com/#/c/344986/2/drivers/gpu/drm/i915/intel_drv.h
    
    BUG=None
    TEST=cat /sys/kernel/debug/dri/*/i915_dmc_info
    TEST=kernel logs show "Finished loading i915/XXX_dmc_verNNN.bin"
    TEST=No graphics regressions
    
    Originally-Reviewed-on: https://chromium-review.googlesource.com/315865
    (cherry picked from commit 2e70b26e64f6 in chromeos-3.18)
    Change-Id: I7ded154b4ae6003d32e87d3a620245a54fcde532
    Reviewed-on: https://chromium-review.googlesource.com/354089
    Commit-Ready: Wayne Boyer <wayne.boyer@intel.com>
    Tested-by: Tarun Vyas <tarun.vyas@intel.com>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Haridhar Kalvala <haridhar.kalvala@intel.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- drivers/gpu/drm/i915/intel_csr.c
+++ drivers/gpu/drm/i915/intel_csr.c
@@ -398,6 +398,7 @@ static uint32_t *parse_csr_fw(struct drm_i915_private *dev_priv,
 
 static void csr_load_work_fn(struct work_struct *work)
 {
+	static const unsigned rootfs_timeout_ms = 60 * 1000;
 	struct drm_i915_private *dev_priv;
 	struct intel_csr *csr;
 	const struct firmware *fw;
@@ -405,6 +406,13 @@ static void csr_load_work_fn(struct work_struct *work)
 
 	dev_priv = container_of(work, typeof(*dev_priv), csr.work);
 	csr = &dev_priv->csr;
+
+	/* Wait until root filesystem is loaded in case the firmware
+	 * is not built-in but in /lib/firmware */
+	WARN(_wait_for(system_state == SYSTEM_RUNNING,
+		       rootfs_timeout_ms * 1000, 100 * 1000),
+	     "Timing out after waiting %dms for SYSTEM_RUNNING",
+	     rootfs_timeout_ms);
 
 	ret = request_firmware(&fw, dev_priv->csr.fw_path,
 			       &dev_priv->dev->pdev->dev);
