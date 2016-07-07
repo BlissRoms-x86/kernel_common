jcadduono/android_kernel_google_chromeos

CHROMIUM: cros_ec: Set driver names properly.
Use cros-ec-<function> driver name convention, regardless of the driver
file name.
Use MODULE_ALIAS when only one type of platform device is exposed by the
driver.

BUG=chromium:617704
TEST=check on chell cros-ec-usbpd-charger as a module is loaded
properly.

Change-Id: I2b87be5395d6ddcaa76265879feb2097889f0a01
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/354777
Reviewed-by: Guenter Roeck <groeck@chromium.org>
Reviewed-by: Brian Norris <briannorris@chromium.org>

drivers/mfd/cros_ec_dev.c
@@ -36,6 +36,8 @@
 
 #include "cros_ec_dev.h"
 
+#define DRV_NAME "cros-ec-dev"
+
 /* Device variables */
 #define CROS_MAX_DEV 128
 static int ec_major;
@@ -388,7 +390,7 @@ static int cros_ec_check_features(struct cros_ec_dev *ec, int feature)
 
 static const struct mfd_cell cros_usb_pd_charger_devs[] = {
 	{
-		.name = "cros_usbpd-charger",
+		.name = "cros-usb-pd-charger",
 		.id   = -1,
 	},
 };
@@ -641,23 +643,14 @@ static const struct dev_pm_ops cros_ec_dev_pm_ops = {
 #endif
 };
 
-static const struct platform_device_id cros_ec_dev_ids[] = {
-	{
-		.name = "cros-ec-dev",
-	},
-	{ /* sentinel */ }
-};
-MODULE_DEVICE_TABLE(platform, cros_ec_dev_ids);
-
 static struct platform_driver cros_ec_dev_driver = {
 	.driver = {
-		.name = "cros-ec-devs",
+		.name = DRV_NAME,
 		.owner = THIS_MODULE,
 		.pm = &cros_ec_dev_pm_ops,
 	},
 	.probe = ec_device_probe,
 	.remove = ec_device_remove,
-	.id_table = cros_ec_dev_ids,
 };
 
 static int __init cros_ec_dev_init(void)
@@ -705,6 +698,7 @@ static void __exit cros_ec_dev_exit(void)
 module_init(cros_ec_dev_init);
 module_exit(cros_ec_dev_exit);
 
+MODULE_ALIAS("platform:" DRV_NAME);
 MODULE_AUTHOR("Bill Richardson <wfrichar@chromium.org>");
 MODULE_DESCRIPTION("Userspace interface to the Chrome OS Embedded Controller");
 MODULE_VERSION("1.0");

BlissRoms-x86/kernel_common

(cherry picked from commit 92fea632a8ea182ca71a2d7b350130cf12ac8e50)
Reviewed-on: https://chromium-review.googlesource.com/358156
Reviewed-by: Guenter Roeck <groeck@google.com>

drivers/platform/chrome/cros_ec_dev.c
@@ -28,6 +28,8 @@
 
 #include "cros_ec_dev.h"
 
+#define DRV_NAME "cros-ec-ctl"
+
 /* Device variables */
 #define CROS_MAX_DEV 128
 static int ec_major;
@@ -532,7 +534,7 @@ static const struct dev_pm_ops cros_ec_dev_pm_ops = {
 
 static struct platform_driver cros_ec_dev_driver = {
 	.driver = {
-		.name = "cros-ec-ctl",
+		.name = DRV_NAME,
 		.pm = &cros_ec_dev_pm_ops,
 	},
 	.probe = ec_device_probe,
@@ -583,6 +585,7 @@ static void __exit cros_ec_dev_exit(void)
 module_init(cros_ec_dev_init);
 module_exit(cros_ec_dev_exit);
 
+MODULE_ALIAS("platform:" DRV_NAME);
 MODULE_AUTHOR("Bill Richardson <wfrichar@chromium.org>");
 MODULE_DESCRIPTION("Userspace interface to the Chrome OS Embedded Controller");
 MODULE_VERSION("1.0");