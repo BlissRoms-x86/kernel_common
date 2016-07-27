2f18f4be6349fbf23d419381cfd8bf96abee8449
Alpha Lin <alpha.lin@rock-chips.com>
2016-07-27 18:46:25 +0800

    CHROMIUM: add v4l2_buffer.config_store to compat-ioctl
    Add v4l2_buffer member field config_store to
    v4l2-compat-ioctl32.c, function get_v4l2_buffer32, in
    order to get right value from 32 bits userspace to 64
    bits kernel.
    
    Change-Id: I9a0110c6a92ac11c2541b3f9a35d63a51a343b85
    Signed-off-by: Alpha Lin <alpha.lin@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/363641
    Commit-Ready: Pawel Osciak <posciak@chromium.org>
    Tested-by: Owen Lin <owenlin@chromium.org>
    Reviewed-by: Owen Lin <owenlin@chromium.org>

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -417,7 +417,8 @@ static int get_v4l2_buffer32(struct v4l2_buffer *kp, struct v4l2_buffer32 __user
 		get_user(kp->type, &up->type) ||
 		get_user(kp->flags, &up->flags) ||
 		get_user(kp->memory, &up->memory) ||
-		get_user(kp->length, &up->length))
+		get_user(kp->length, &up->length) ||
+		get_user(kp->config_store, &up->config_store))
 			return -EFAULT;
 
 	if (V4L2_TYPE_IS_OUTPUT(kp->type))
