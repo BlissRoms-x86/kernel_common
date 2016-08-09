03265d08bf319aa474039d088c1a10ee0cf144eb
Derek Basehore <dbasehore@chromium.org>
2016-08-09 12:43:33 -0700

    CHROMIUM: cpufreq: Max freq invariant scheduler load-tracking and cpu capacity support
    Implements cpufreq_scale_max_freq_capacity() to provide the scheduler
    with a maximum frequency scaling correction factor for more accurate
    load-tracking and cpu capacity handling by being able to deal with
    frequency capping.
    
    This scaling factor describes the influence of running a cpu with a
    current maximum frequency lower than the absolute possible maximum
    frequency on load tracking and cpu capacity.
    
    The factor is:
    
    	current_max_freq(cpu) << SCHED_CAPACITY_SHIFT / max_freq(cpu)
    
    In fact, max_freq_scale should be a struct cpufreq_policy data member.
    But this would require that the scheduler hot path (__update_load_avg())
    would have to grab the cpufreq lock. This can be avoided by using per-cpu
    data initialized to SCHED_CAPACITY_SCALE for max_freq_scale.
    
    BUG=chromium:580187
    TEST=boot kevin/gru
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    
    Changed from using the / operator to using do_div (fixed arm 32 bit).
    
    Change-Id: I609d2269d56740aa4357e535c085d93eced0eb83
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/358237
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -352,12 +352,14 @@ static void adjust_jiffies(unsigned long val, struct cpufreq_freqs *ci)
  *********************************************************************/
 
 static DEFINE_PER_CPU(unsigned long, freq_scale) = SCHED_CAPACITY_SCALE;
+static DEFINE_PER_CPU(unsigned long, max_freq_scale) = SCHED_CAPACITY_SCALE;
 
 static void
 scale_freq_capacity(struct cpufreq_policy *policy, struct cpufreq_freqs *freqs)
 {
 	unsigned long long cur = freqs ? freqs->new : policy->cur;
 	unsigned long long scale = cur << SCHED_CAPACITY_SHIFT;
+	struct cpufreq_cpuinfo *cpuinfo = &policy->cpuinfo;
 	int cpu;
 
 	do_div(scale, policy->max);
@@ -366,11 +368,29 @@ scale_freq_capacity(struct cpufreq_policy *policy, struct cpufreq_freqs *freqs)
 
 	for_each_cpu(cpu, policy->cpus)
 		per_cpu(freq_scale, cpu) = (unsigned long)scale;
+
+	if (freqs)
+		return;
+
+	scale = (unsigned long long)policy->max << SCHED_CAPACITY_SHIFT;
+	do_div(scale, cpuinfo->max_freq);
+
+	pr_debug("cpus %*pbl cur max/max freq %u/%u kHz max freq scale %llu\n",
+		 cpumask_pr_args(policy->cpus), policy->max, cpuinfo->max_freq,
+		 scale);
+
+	for_each_cpu(cpu, policy->cpus)
+		per_cpu(max_freq_scale, cpu) = (unsigned long)scale;
 }
 
 unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu)
 {
 	return per_cpu(freq_scale, cpu);
+}
+
+unsigned long cpufreq_scale_max_freq_capacity(int cpu)
+{
+	return per_cpu(max_freq_scale, cpu);
 }
 
 static void __cpufreq_notify_transition(struct cpufreq_policy *policy,
