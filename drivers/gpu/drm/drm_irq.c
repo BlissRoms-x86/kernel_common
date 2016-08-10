314a039e0e017ad97b1e3a590f3ebc271aa232ad
Sean Paul <seanpaul@chromium.org>
2016-08-09 21:00:08 -0400

    CHROMIUM: drm: Reduce default vblank off time to 50ms
    5 seconds is too conservative, let's use 50ms which is
    what we use on every other kernel version.
    
    BUG=None
    TEST=Boot kevin, ensure psr triggers every 50ms
    
    Change-Id: Ie30dd38a1c575882479e26ed3a7931f3b942f0e0
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/367606
    Tested-by: Haixia Shi <hshi@chromium.org>
    Reviewed-by: Haixia Shi <hshi@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Haixia Shi <hshi@chromium.org>

--- drivers/gpu/drm/drm_irq.c
+++ drivers/gpu/drm/drm_irq.c
@@ -68,7 +68,7 @@ static unsigned int drm_timestamp_precision = 20;  /* Default to 20 usecs. */
  */
 unsigned int drm_timestamp_monotonic = 1;
 
-static int drm_vblank_offdelay = 5000;    /* Default to 5000 msecs. */
+static int drm_vblank_offdelay = 50;    /* Default to 50 msecs. */
 
 module_param_named(vblankoffdelay, drm_vblank_offdelay, int, 0600);
 module_param_named(timestamp_precision_usec, drm_timestamp_precision, int, 0600);
