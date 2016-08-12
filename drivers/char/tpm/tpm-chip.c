BlissRoms-x86/kernel_common

commit d479a09355251cd66d318396a38c3d26bd893f3b

CHROMIUM: workaround for Infineon TPM defensive timeout (v2)
The SLB9635 TPM by Infineon goes into a self-defense mode when
power is cut in the middle of a command too many times, and getting
it out of that mode is difficult and takes a long time.  This patch
adds a shutdown-time callback that grabs the mutex wrapping a
request-response transaction, so it's less likely that we'll lose
power in the middle of a command.

Note that the TPM may lose power also across suspend/resume (depending
on hardware).  However, the suspend callback sends a SaveState command,
which can only be sent when other running commands have completed.
I am not sure if a command can start after the suspend callback
has completed (I doubt it) but even if it does, it will be
a sufficiently rare event, and the self-defense can tolerate that.

The "gentle shutdown" message is added to make it easier to detect
if we neglect to carry this patch forward.

BUG=chromium:234796
TEST=extensively tested with the librarian simulator
Signed-off-by: Luigi Semenzato <semenzato@chromium.org>

Change-Id: I1bb0a6d574c96a33f4344d47e3e7584e3f67fb14
Reviewed-on: https://gerrit.chromium.org/gerrit/58902
Tested-by: Luigi Semenzato <semenzato@chromium.org>
Reviewed-by: Darren Krahn <dkrahn@chromium.org>
Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
Reviewed-by: Olof Johansson <olofj@chromium.org>
[benzh: 3.14 rebase. tpm.c has been split into multiple files.]
Signed-off-by: Ben Zhang <benzh@chromium.org>
[bfreed: Commit the change reverted to avoid conflicts in upstream patches.]
Signed-off-by: Bryan Freed <bfreed@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/316574
Reviewed-by: Luigi Semenzato <semenzato@chromium.org>

[rebase44(groeck): Change moved from drivers/char/tpm/tpm_tis_common.c
	to drivers/char/tpm/tpm_tis.c]
Signed-off-by: Guenter Roeck <groeck@chromium.org>

drivers/char/tpm/tpm-chip.c
@@ -25,6 +25,7 @@
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
 #include <linux/freezer.h>
+#include <linux/reboot.h>
 #include <linux/major.h>
 #include "tpm.h"
 #include "tpm_eventlog.h"
@@ -71,6 +72,7 @@ static void tpm_dev_release(struct device *dev)
 	spin_lock(&driver_lock);
 	clear_bit(chip->dev_num, dev_mask);
 	spin_unlock(&driver_lock);
+	unregister_reboot_notifier(&chip->shutdown_nb);
 	kfree(chip);
 }
 
@@ -201,6 +203,16 @@ static void tpm1_chip_unregister(struct tpm_chip *chip)
 	tpm_sysfs_del_device(chip);
 }
 
+static int tpm_shutdown_notify(struct notifier_block *nb,
+				unsigned long unused, void *unused2)
+{
+	struct tpm_chip *chip = container_of(nb, struct tpm_chip, shutdown_nb);
+	dev_dbg(&chip->dev, "acquiring shutdown lock\n");
+	mutex_lock(&chip->tpm_mutex);
+	dev_dbg(&chip->dev, "acquired shutdown lock\n");
+	return NOTIFY_DONE;
+}
+
 /*
  * tpm_chip_register() - create a character device for the TPM chip
  * @chip: TPM chip to use.
@@ -230,6 +242,13 @@ int tpm_chip_register(struct tpm_chip *chip)
 	spin_lock(&driver_lock);
 	list_add_tail_rcu(&chip->list, &tpm_chip_list);
 	spin_unlock(&driver_lock);
+
+	/* INFINEON TPM WORKAROUND: Register shutdown callback that ensures we
+	 * don't shut down in the middle of a TPM command, or we may trigger
+	 * nasty defensive timeouts at the next boot.
+	 */
+	chip->shutdown_nb.notifier_call = tpm_shutdown_notify;
+	register_reboot_notifier(&chip->shutdown_nb);
 
 	chip->flags |= TPM_CHIP_FLAG_REGISTERED;
 

drivers/char/tpm/tpm.h
@@ -197,6 +197,7 @@ struct tpm_chip {
 #endif /* CONFIG_ACPI */
 
 	struct list_head list;
+	struct notifier_block shutdown_nb;
 };
 
 #define to_tpm_chip(d) container_of(d, struct tpm_chip, dev)

drivers/char/tpm/tpm_tis.c
@@ -691,7 +691,7 @@ static int tpm_tis_init(struct device *dev, struct tpm_info *tpm_info,
 	vendor = ioread32(chip->vendor.iobase + TPM_DID_VID(0));
 	chip->vendor.manufacturer_id = vendor;
 
-	dev_info(dev, "%s TPM (device-id 0x%X, rev-id %d)\n",
+	dev_info(dev, "%s TPM (device-id 0x%X, rev-id %d) [gentle shutdown]\n",
 		 (chip->flags & TPM_CHIP_FLAG_TPM2) ? "2.0" : "1.2",
 		 vendor >> 16, ioread8(chip->vendor.iobase + TPM_RID(0)));
 

commit 8dd8f10effe9e9b76cbe8f5f36a7702470d5e981

FIXUP: CHROMIUM: TPM: log "gentle shutdown" notice on all platforms.
We logged this on x86 only, thus making kernel_TPMPing fail on ARM.

BUG=chromium:351054
TEST=none
BRANCH=none
Signed-off-by: Luigi Semenzato <semenzato@chromium.org>

Original-Change-Id: I0dab38bd87e6d925bda6b5302b8f8b1014e84a75
Originally-Reviewed-on: https://chromium-review.googlesource.com/189449
Tested-by: Luigi Semenzato <semenzato@chromium.org>
Reviewed-by: Olof Johansson <olofj@chromium.org>
Commit-Queue: Luigi Semenzato <semenzato@chromium.org>

[This wasn't picked into v3.14, etc.; I guess no one is running
kernel_TPMPing ?]

TEST=kernel_TPMPing
BUG=chromium:634341

Change-Id: Ib71da0ffd6c88967ee0134dc595ea995bea3a9e9
Fixes: d479a0935525 ("CHROMIUM: workaround for Infineon TPM defensive timeout (v2)")
Signed-off-by: Brian Norris <computersforpeace@gmail.com>
Reviewed-on: https://chromium-review.googlesource.com/366182
Commit-Ready: Brian Norris <briannorris@chromium.org>
Tested-by: Brian Norris <briannorris@chromium.org>
Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
Reviewed-by: Douglas Anderson <dianders@chromium.org>

drivers/char/tpm/tpm-chip.c
@@ -393,6 +393,7 @@ int tpm_chip_register(struct tpm_chip *chip)
 	 * nasty defensive timeouts at the next boot.
 	 */
 	chip->shutdown_nb.notifier_call = tpm_shutdown_notify;
+	dev_info(&chip->dev, "registering reboot notifier [gentle shutdown]\n");
 	register_reboot_notifier(&chip->shutdown_nb);
 
 	chip->flags |= TPM_CHIP_FLAG_REGISTERED;

drivers/char/tpm/tpm_tis_core.c
@@ -717,7 +717,7 @@ int tpm_tis_core_init(struct device *dev, struct tpm_tis_data *priv, int irq,
 	if (rc < 0)
 		goto out_err;
 
-	dev_info(dev, "%s TPM (device-id 0x%X, rev-id %d) [gentle shutdown]\n",
+	dev_info(dev, "%s TPM (device-id 0x%X, rev-id %d)\n",
 		 (chip->flags & TPM_CHIP_FLAG_TPM2) ? "2.0" : "1.2",
 		 vendor >> 16, rid);
 