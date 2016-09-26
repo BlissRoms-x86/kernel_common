b3cdd10d4e8c864521ff2d209cd3e3625deab989
Duncan Laurie <dlaurie@chromium.org>
2016-09-26 09:45:53 -0700

    Revert "CHROMIUM: i2c / ACPI : use i2c_new_probed_device"
    This reverts commit 201eb00d592f
    ("CHROMIUM: i2c / ACPI : use i2c_new_probed_device")
    
    Changing the default behavior for I2C devices added by ACPI is
    causing problems for I2C devcies that cannot tolerate this type
    of transaction.
    
    Rather than impact all devices with this change I will introduce
    a new device property that firmware can use to indicate to the
    OS that it should probe the device before adding.
    
    BUG=chrome-os-partner:57686
    TEST=successfully use H1 TPM via I2C on reef after booting kernel
    
    Change-Id: I955e82d2bff98ccb995978622e75414601188097
    Reviewed-on: https://chromium-review.googlesource.com/388766
    Commit-Ready: Duncan Laurie <dlaurie@chromium.org>
    Tested-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -147,7 +147,6 @@ static acpi_status acpi_i2c_add_device(acpi_handle handle, u32 level,
 	struct i2c_board_info info;
 	struct acpi_device *adev;
 	int ret;
-	unsigned short addrs[] = {0, I2C_CLIENT_END};
 
 	if (acpi_bus_get_device(handle, &adev))
 		return AE_OK;
@@ -189,9 +188,8 @@ static acpi_status acpi_i2c_add_device(acpi_handle handle, u32 level,
 	acpi_dev_free_resource_list(&resource_list);
 
 	adev->power.flags.ignore_parent = true;
-	addrs[0] = info.addr;
 	strlcpy(info.type, dev_name(&adev->dev), sizeof(info.type));
-	if (!i2c_new_probed_device(adapter, &info, addrs, NULL)) {
+	if (!i2c_new_device(adapter, &info)) {
 		adev->power.flags.ignore_parent = false;
 		dev_err(&adapter->dev,
 			"failed to add I2C device %s from ACPI\n",
