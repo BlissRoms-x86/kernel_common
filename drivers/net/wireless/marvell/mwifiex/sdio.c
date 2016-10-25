73219362d1625f2a3ea0d9af0c9cf190852ea742
Brian Norris <briannorris@chromium.org>
2016-10-25 16:20:00 -0700

    CHROMIUM: mwifiex: don't pretend to resume while remove()'ing
    The device core will not allow suspend() to race with remove().
    
    BUG=chrome-os-partner:58129, chrome-os-partner:58291
    TEST=suspend/reboot tests
    
    Change-Id: Ibe811ad4388bd3acfdcb9503d3af8eeb06d928b1
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/403531
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- drivers/net/wireless/marvell/mwifiex/sdio.c
+++ drivers/net/wireless/marvell/mwifiex/sdio.c
@@ -287,9 +287,6 @@ mwifiex_sdio_remove(struct sdio_func *func)
 	mwifiex_dbg(adapter, INFO, "info: SDIO func num=%d\n", func->num);
 
 	if (user_rmmod) {
-		if (adapter->is_suspended)
-			mwifiex_sdio_resume(adapter->dev);
-
 		mwifiex_deauthenticate_all(adapter);
 
 		priv = mwifiex_get_priv(adapter, MWIFIEX_BSS_ROLE_ANY);
