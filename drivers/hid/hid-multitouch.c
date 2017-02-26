b0baf85968d529209be8029e4740d5a6503ad77a
electrikjesus <electrikjesus@gmail.com>
2017-02-26 15:13:28 -0500

    Redefine Multitouch quirks

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -170,7 +170,6 @@ static void mt_post_parse(struct mt_device *td);
  * these device-dependent functions determine what slot corresponds
  * to a valid contact that was just read.
  */
-
 static int cypress_compute_slot(struct mt_device *td)
 {
 	if (td->curdata.contactid != 0 || td->num_received == 0)
@@ -275,6 +274,11 @@ static struct mt_class mt_classes[] = {
 	},
 	{ .name = MT_CLS_NTRIG,
 		.quirks = MT_QUIRK_ALWAYS_VALID |
+			MT_QUIRK_INVALID_CONTACTID_FFFF,
+	},
+	{ .name = MT_CLS_NTRIG,
+		.quirks	= MT_QUIRK_NOT_SEEN_MEANS_UP |
+			MT_QUIRK_ALWAYS_VALID |
 			MT_QUIRK_INVALID_CONTACTID_FFFF,
 	},
 	{ }
@@ -619,6 +623,10 @@ static int mt_compute_slot(struct mt_device *td, struct input_dev *input)
 	    td->curdata.contactid == 0xffff)
 		return -1;
 
+	if ((quirks & MT_QUIRK_INVALID_CONTACTID_FFFF) &&
+	    td->curdata.contactid == 0xffff)
+		return -1;
+
 	return input_mt_get_slot_by_key(input, td->curdata.contactid);
 }
 
@@ -641,6 +649,12 @@ static void mt_complete_slot(struct mt_device *td, struct input_dev *input)
 		int slotnum = mt_compute_slot(td, input);
 		struct mt_slot *s = &td->curdata;
 		struct input_mt *mt = input->mt;
+
+		if ((td->mtclass.quirks & MT_QUIRK_INVALID_CONTACTID_FFFF) &&
+		    s->contactid == 0xffff) {
+			td->num_received++;
+			return;
+		}
 
 		if (slotnum < 0 || slotnum >= td->maxcontacts)
 			return;
@@ -758,7 +772,6 @@ static void mt_process_mt_event(struct hid_device *hid, struct hid_field *field,
 		case HID_DG_TOUCH:
 			/* do nothing */
 			break;
-
 		default:
 			if (usage->type)
 				input_event(input, usage->type, usage->code,
@@ -1069,6 +1082,7 @@ static int mt_input_configured(struct hid_device *hdev, struct hid_input *hi)
 		case HID_CP_CONSUMER_CONTROL:
 			suffix = "Consumer Control";
 			break;
+
 		default:
 			suffix = "UNKNOWN";
 			break;
