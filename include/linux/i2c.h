212691b62f3927d81a63b08f963e8033d15d0536
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2017-03-04 11:29:34 -0800

    i2c: export i2c_client_type structure
    i2c bus has 2 different types of device belonging to the same bus and
    bus notifiers use device type to distinguish between adapters and clients.
    Previously we only had i2c_adapter_type exported, which made code wanting
    to work with i2c_client devices test for type not equal to adapter type.
    This unfortunately is not safe if we ever add another type to the bus,
    so let's export i2c_client_type as well.
    
    Reviewed-by: Jean Delvare <jdelvare@suse.de>
    Acked-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Reviewed-by: Wolfram Sang <wsa@the-dreams.de>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: youling257 <youling257@gmail.com>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -36,6 +36,7 @@
 
 extern struct bus_type i2c_bus_type;
 extern struct device_type i2c_adapter_type;
+extern struct device_type i2c_client_type;
 
 /* --- General options ------------------------------------------------	*/
 
