124a19a8cb51638d68eafc6ee7da0c2f5b8586e4
youling257 <youling257@gmail.com>
2017-12-01 20:05:02 +0800

    Revert "ANDROID: Paranoid network."
    Signed-off-by: youling257 <youling257@gmail.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -124,9 +124,6 @@
 
 #include <trace/events/sock.h>
 
-#ifdef CONFIG_ANDROID_PARANOID_NETWORK
-#include <linux/android_aid.h>
-#endif
 
 /* The inetsw table contains everything that inet_create needs to
  * build a new socket.
@@ -244,29 +241,6 @@ out:
 }
 EXPORT_SYMBOL(inet_listen);
 
-#ifdef CONFIG_ANDROID_PARANOID_NETWORK
-static inline int current_has_network(void)
-{
-	return (!current_euid() || in_egroup_p(AID_INET) ||
-		in_egroup_p(AID_NET_RAW));
-}
-static inline int current_has_cap(struct net *net, int cap)
-{
-	if (cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW))
-		return 1;
-	return ns_capable(net->user_ns, cap);
-}
-# else
-static inline int current_has_network(void)
-{
-	return 1;
-}
-static inline int current_has_cap(struct net *net, int cap)
-{
-	return ns_capable(net->user_ns, cap);
-}
-#endif
-
 /*
  *	Create an inet socket.
  */
@@ -284,9 +258,6 @@ static int inet_create(struct net *net, struct socket *sock, int protocol,
 
 	if (protocol < 0 || protocol >= IPPROTO_MAX)
 		return -EINVAL;
-
-	if (!current_has_network())
-		return -EACCES;
 
 	sock->state = SS_UNCONNECTED;
 
@@ -337,7 +308,7 @@ lookup_protocol:
 
 	err = -EPERM;
 	if (sock->type == SOCK_RAW && !kern &&
-	    !current_has_cap(net, CAP_NET_RAW))
+	    !ns_capable(net->user_ns, CAP_NET_RAW))
 		goto out_rcu_unlock;
 
 	sock->ops = answer->ops;
