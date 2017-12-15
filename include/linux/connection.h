b7836b762fa814e47bf54299bbd97696219cccb7
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2017-12-15 17:00:15 +0300

    drivers: base: Unified device connection lookup
    Several frameworks - clk, gpio, phy, pmw, etc. - maintain
    lookup tables for describing connections and provide custom
    API for handling them. This introduces a single generic
    lookup table and API for the connections.
    
    The motivation for this commit is centralizing the
    connection lookup, but the goal is to ultimately extract the
    connection descriptions also from firmware by using the
    fwnode_graph_* functions and other mechanisms that are
    available.
    
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>

---
+++ include/linux/connection.h
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#ifndef _LINUX_CONNECTION_H_
+#define _LINUX_CONNECTION_H_
+
+#include <linux/list.h>
+
+struct device;
+
+/**
+ * struct devcon - Device Connection Descriptor
+ * @endpoint: The names of the two devices connected together
+ * @id: Unique identifier for the connection
+ */
+struct devcon {
+	const char		*endpoint[2];
+	const char		*id;
+	struct list_head	list;
+};
+
+void *__device_find_connection(struct device *dev, const char *con_id,
+			       void *data,
+			       void *(*match)(struct devcon *con, int ep,
+					      void *data));
+
+struct device *device_find_connection(struct device *dev, const char *con_id);
+
+#define DEVCON(_ep0, _ep1, _id)		{ { _ep0, _ep1 }, _id, }
+
+void add_device_connection(struct devcon *con);
+void remove_device_connection(struct devcon *con);
+
+#endif /* _LINUX_CONNECTION_H_ */
