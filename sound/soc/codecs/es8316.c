f92b65c929ba62cb8f27a01c30b1dd155943d4d5
GuneetAtwal <guneetatwal@gmail.com>
2018-09-04 19:02:48 +0530

    ASoC:es8316: Add missing header this patch fixes the irq_get_trigger_type implicit warning error
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- sound/soc/codecs/es8316.c
+++ sound/soc/codecs/es8316.c
@@ -16,6 +16,7 @@
 #include <linux/acpi.h>
 #include <linux/delay.h>
 #include <linux/i2c.h>
+#include <linux/irq.h>
 #include <linux/mod_devicetable.h>
 #include <linux/regmap.h>
 #include <sound/pcm.h>
