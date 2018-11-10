7d39716a89294dd476245a33b9bac236921212a3
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-11-09 19:59:31 -0500

    ASoC: Intel: add support for Realtek 5651 on Cherrytrail
    RT5651 is used on some Cherrytrail platforms, add the ACPI
    ID in machine table.
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=156191
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    ---
     sound/soc/intel/atom/sst/sst_acpi.c | 4 +++-
     1 file changed, 3 insertions(+), 1 deletion(-)

--- sound/soc/intel/atom/sst/sst_acpi.c
+++ sound/soc/intel/atom/sst/sst_acpi.c
@@ -532,7 +532,9 @@ static struct sst_acpi_mach sst_acpi_chv[] = {
 	/* some CHT-T platforms rely on RT5640, use Baytrail machine driver */
 	{"10EC5640", "bytcr_rt5640", "intel/fw_sst_22a8.bin", "bytcr_rt5640", cht_quirk,
 						&chv_platform_data },
-
+	/* some CHT-T platforms rely on RT5651, use Baytrail machine driver */
+	{"10EC5651", "bytcr_rt5651", "intel/fw_sst_22a8.bin", "bytcr_rt5651", NULL,
+						&chv_platform_data },
 	{},
 };
 
