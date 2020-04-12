f518e3e56401f6348782445969efd791c1180946
hmtheboy154 <buingoc67@gmail.com>
2020-04-12 14:13:12 +0700

    Adding SupremeGamers and Teri logo

--- include/linux/linux_logo.h
+++ include/linux/linux_logo.h
@@ -45,6 +45,8 @@ extern const struct linux_logo logo_superh_mono;
 extern const struct linux_logo logo_superh_vga16;
 extern const struct linux_logo logo_superh_clut224;
 extern const struct linux_logo logo_spe_clut224;
+extern const struct linux_logo logo_supremegamers_clut224;
+extern const struct linux_logo logo_teri_clut224;
 
 extern const struct linux_logo *fb_find_logo(int depth);
 #ifdef CONFIG_FB_LOGO_EXTRA
