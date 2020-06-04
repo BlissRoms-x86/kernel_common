6ea9cd07fffa412a0a4fe943909dbc0d7eda5f78
saranya <saranya.gopal@intel.com>
2020-06-04 14:17:06 +0530

    uvc: Add UVC GET_DEF probe quirk for C922 camera
    In some of the C922 camera devices, we find that
    GET_DEF request is not implemented for video probe
    and commit controls. These cameras crash when this
    request is received. Hence, add this quirk for C922
    cameras.
    
    Tracked-On: OAM-91289
    Signed-off-by: saranya <saranya.gopal@intel.com>

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -2492,6 +2492,15 @@ static const struct usb_device_id uvc_ids[] = {
 	  .bInterfaceSubClass	= 1,
 	  .bInterfaceProtocol	= 0,
 	  .driver_info		= UVC_INFO_QUIRK(UVC_QUIRK_RESTORE_CTRLS_ON_INIT) },
+	/* Logitech HD Pro Webcam C922 */
+	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
+				| USB_DEVICE_ID_MATCH_INT_INFO,
+	  .idVendor		= 0x046d,
+	  .idProduct		= 0x085c,
+	  .bInterfaceClass	= USB_CLASS_VIDEO,
+	  .bInterfaceSubClass	= 1,
+	  .bInterfaceProtocol	= 0,
+	  .driver_info		= (kernel_ulong_t)&uvc_quirk_probe_def },
 	/* Chicony CNF7129 (Asus EEE 100HE) */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
 				| USB_DEVICE_ID_MATCH_INT_INFO,
